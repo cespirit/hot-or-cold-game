d712a868423084abc543087bccd713fe03567ce6
cespirit <ckespir@gmail.com>
2014-12-12 19:57:06 -0800

    Valid input check and new game resets working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 
 $(document).ready(function(){
-	
+
+	var min;
+	var max;
+	var number;
+	var guessCount;
+
+	newGame();
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +19,73 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*--- Run a new game ---*/
+  	$(".new").click(function(){
+  		newGame();
+  	});
+
+  	/*--- Display guess feedback ---*/
+  	//$("#guess-form").submit({min: min, max: max}, getFeedBack);
+  	$("#guess-form").submit(getFeedBack);
+
 });
 
+/* http://stackoverflow.com/questions/1527803/generating-random-numbers-in-javascript-in-a-specific-range */
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
+function newGame() {
+	$("#feedback").html("Make your Guess!");
+	$("#guessList").html("");
+	guessCount = 0;
+	$("#count").html(guessCount);
+
+	min = 1;
+	max = 100;
+	number = getRandomInt(min, max);
+
+	console.log("New Game Number: ", number);
+}
+
+function isGuessValid(guess) {
+	if(guess.indexOf(".") != -1){
+		return false;
+	}
+	
+	guess = +$.trim(guess);
+	
+	if(!guess || guess < min || guess > max ) {		
+		return false;		
+	}	
+		
+	return true;
+}
+
+function updateGuessCount(count) {
+	guessCount = count;
+	$("#count").html(guessCount);
+}
+
+function getFeedBack(event) {
+	event.preventDefault();
+	var guess = $("#userGuess").val();	
+
+	if(isGuessValid(guess)) {
+		console.log("Valid Guess: ", guess);
+		
+		guessCount++;
+		updateGuessCount(guessCount);
+		//console.log(guessCount++);
+	}
+	else {
+		$("#feedback").fadeOut("fast");
+		$("#feedback").html("You entered &#39;" + guess + "&#39;. <br> Please enter a number from " + min + " to " + max + ".");
+		$("#feedback").fadeIn("fast");
+	}
+
+	// Set ranges for values (warm or cold) 
+	// Don't forget edge cases
+
+	$("#userGuess").val("");
+}
